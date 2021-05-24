361fd432e13af36867dba213a3ae65ea601a3d7c
Sherry Joseph <sherryjoseph@Sherrys-MacBook-Air.local>
2021-05-24 17:14:22 -0400

    mon commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,51 @@
 /*----- constants -----*/
 //variables for what I want to target
 const $name = $('#name');
-const $description = $('description');
+const $description = $('#description');
+//const $input = $('input[type="text"]')
 
 
 /*----- app's state (variables) -----*/
 let exerciseData; //global variable that we can access anywhere below
 
 
-
-
 /*----- cached element references -----*/
 const $mainContent = $('main');
 
 /*----- event listeners -----*/
+$('form').on('submit', handleGetData);
+
 /*----- functions -----*/
 
-function handleGetData (evt) {
-    evt.preventDefault();
+function render() {
+    exerciseData.results.map(function(exercise, idx) {
+        return `
+        $name.text = ${exercise.name}
+        $description.text = ${exercise.description}`
+    });
+
+    // $name.text(exerciseData.object.keys(results[3]));
+    // $description.text(exerciseData.object.keys(results[6]));
+    // $name.text(exerciseData.results[0].name);
+    // $description.text(exerciseData.results[0].description);
+
+}
+
+
+function handleGetData(event) {
+    event.preventDefault(); //want to prevent a page refresh by calling prventDefault() on 'submit'
+
+    let exploreText = $('input#explore').val(); // getting the value/result from the user input here
+
     $.ajax({
-            url: "https://wger.de/api/v2/exercise/"
+            url: `https://wger.de/api/v2/exercise/?format=json&limit=400&?=${exploreText}`,
         })
         .then(
             function (data) {
                 exerciseData = data;
                 render();
+                exerciseData = "";
+
 
             },
             function (error) {
@@ -34,7 +55,33 @@ function handleGetData (evt) {
         );
 }
 
-function render() {
-    $name.text(exerciseData.name);
-    $description.text(exerciseData.description);
-}
+
+
+
+// let reformattedArray = kvArray.map(function(obj) {
+//     let rObj = {}
+//     rObj[obj.key] = obj.value
+//     return rObj
+// })
+
+
+// for(var i in jsonData){
+//     var key = i;
+//     var val = jsonData[i];
+//     for(var j in val){
+//         var sub_key = j;
+//         var sub_val = val[j];
+//         console.log(sub_key);
+//     }
+// }
+// success: function(data){
+//     $.each(data.results, function(index, object){
+//         $("body").append("<div>"+object.name +"</div><br/>")
+//     })
+// }
+// response($.map(data.results, function(index,item) {
+//     return {
+//         label: item.name,
+//         value: item.name
+
+       
