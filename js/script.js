728a9e6f17cee1d3dad9e3e45e8a5ef0f0de5c4d
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-05-26 09:52:06 -0400

    wireframe update

--- js/script.js
+++ js/script.js
@@ -1,29 +1,23 @@
 /*----- constants -----*/
-//variables for what I want to target
-const $name = $('#name');
-const $description = $('#description');
-
 /*----- app's state (variables) -----*/
 let exerciseData; //global variable that we can access anywhere below
  // getting the value/result from the user input here
- 
 
 /*----- cached element references -----*/
 const $mainContent = $('main');
+const $modal = $('.modal');
 
 /*----- event listeners -----*/
-// $('form').on('submit', handleGetData);
+$mainContent.on('click', 'article', handleClick);//this event handler will only get caalled when a card is clicked
 
 /*----- functions -----*/
-
  // render() responsible for visualizing
-//.find will iterate through results and return the first result that matches the userInput. 
 // the results variable is referencing the array and result is representing each element within the array 
 
 function render() { 
     const html = exerciseData.results.map(function(result) {
         return `
-        <article>
+        <article data-exerise-name"${result.name}">
         <h3>${result.name}</h3>
         </article>
         `;
@@ -31,37 +25,40 @@ function render() {
     $mainContent.append(html);
  };
 
- 
-// forEach
-//  exerciseData.results.forEach(function(result) {
-//     if (result === exerciseData.name) {   
-//         $name.text(exerciseData.results.name);
-//     }
-
-// })
-
 getApiData();
-//want to prevent a page refresh by calling prventDefault() on 'submit'
     function getApiData() {
-        // preventDefault(); 
         $.ajax({
-                url: "https://wger.de/api/v2/exercise/?format=json&limit=20&language=2",
-                
+                url: "https://wger.de/api/v2/exercise/?format=json&limit=16&language=2",
             })
-            .then(
-                function (data) {
+            .then(function (data) {
                     exerciseData = data;
                     render();
-                    // exerciseData = "";
                 },
                 function (error) {
                     console.log('bad request: ', error);
                 }
-                
             );    
     }
 
-   
+function handleClick() {
+    const exerciseName = this.dataset.exerciseName
+     
+    const outcome = exerciseData.results.find(function(result) {
+         return result.name === exerciseName;
+         
+     });
+     console.log(outcome);
+
+
+        const html = `
+        <div>
+            <h3>${exerciseData.results.name}</h3>
+            <p>${exerciseData.results.description}</p>
+            </div>
+        `;
+
+        $modal.html(html).modal();
+};
 
 
 
@@ -72,14 +69,7 @@ getApiData();
 
 
 
-    // let userInput = $('input#search').val();
-    // let apiMatchWithUserInput = exerciseData.results.find(function(result) { 
-    //     if (result.name === userInput) {
-    //        $name.text(apiMatchWithUserInput.name);
-    //        $description.text(apiMatchWithUserInput.description);
-    //     };
-        
-    // });
+
   
 
 
