aa0f4f2cf472e49956dca95a71d606d47710b4c5
Sherry Joseph <sherryjoseph@Sherrys-MacBook-Air.local>
2021-05-27 18:47:30 -0400

    mobile version in css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let exerciseData; //global variable that we can access anywhere below
- // getting the value/result from the user input here
+// getting the value/result from the user input here
 
 /*----- cached element references -----*/
 const $mainContent = $('main');
@@ -12,61 +12,53 @@ const $modal = $('.modal');
 $mainContent.on('click', 'article', handleClick);
 
 /*----- functions -----*/
- // render() responsible for visualizing
+// render() responsible for visualizing
 // the results variable is referencing the array and result is representing each element within the array 
 
-function render() { 
-    let html = exerciseData.results.map(function(result) {
-        
+function render() {
+    let html = exerciseData.results.map(function (result) {
+
         return `
-        <article data-exercise-name="${result.name}" ">
+        <article data-exercise-name='${result.name}' >
         <h3>${result.name}</h3>
         </article>`;
     });
 
     $mainContent.append(html);
- }
-
-
-getApiData();
-    function getApiData() {
-        $.ajax({
-                url: "https://wger.de/api/v2/exercise/?format=json&limit=20&language=2",
-            })
-            .then(function (data) {
-                    exerciseData = data;
-                    render();
-                },
-                function (error) {
-                    console.log('bad request: ', error);
-                }
-            );    
-    }
-
-function handleClick() {
-    const exerciseName = this.dataset.exerciseName;
- 
-    const outcome = exerciseData.results.find(function(result) {
-         return result.name === exerciseName;
-         
-     });
-     
-        const html = `
-        <div>
-            <h3><strong>${outcome.name}</strong></h3>
-            <p id="description">${outcome.description}</p>
-            </div>
-        `;
-
-        $modal.html(html).modal();
-       
 }
 
 
+getApiData();
 
+function getApiData() {
+    $.ajax({
+            url: 'https://wger.de/api/v2/exercise/?format=json&limit=20&language=2',
+        })
+        .then(function (data) {
+                exerciseData = data;
+                render();
+            },
+            function (error) {
+                console.log('bad request: ', error);
+            }
+        );
+}
 
+function handleClick() {
+    const exerciseName = this.dataset.exerciseName;
 
+    const outcome = exerciseData.results.find(function (result) {
+        return result.name === exerciseName;
 
+    });
 
+    const html = `
+        <div>
+            <h3><strong>${outcome.name}</strong></h3>
+            <p id='description'>${outcome.description}</p>
+            </div>
+        `;
 
+    $modal.html(html).modal();
 
+}
